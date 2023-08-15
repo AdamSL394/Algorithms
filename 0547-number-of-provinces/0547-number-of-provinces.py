06d89b8b7884effa23e482f4ec67a128435196f7
Adam Lehrer <50192895+AdamSL394@users.noreply.github.com>
2023-08-15 14:13:08 -0700

    Time: 258 ms (13.83%), Space: 17.2 MB (63.56%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        N = len(isConnected[0])
+        G = {}
+        
+        for node in range(len(isConnected)):
+            level = node + 1
+            for i,v in enumerate(isConnected[node]):
+                if level not in G:
+                    G[level] = []
+                if v == 1:
+                    G[level].append(i + 1)
+                    
+        stack = []
+        
+        seen = set()
+        count = 0
+
+        for i,v in enumerate(isConnected[0]):
+            level = i + 1
+            stack.append(level)
+            if level not in seen:
+                while stack:
+                    node = stack.pop(0)
+                    seen.add(node)
+                    for nei in range(len(G[node])):
+                        if (G[node])[nei] not in seen:
+                            stack.append((G[node])[nei])
+                count += 1
+        
+
+        return count
