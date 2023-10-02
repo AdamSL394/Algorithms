ab2e5caee525217f7635ba5ddf4939ae85676aa1
Adam Lehrer <50192895+AdamSL394@users.noreply.github.com>
2023-10-01 21:38:56 -0700

    Time: 259 ms (23.72%), Space: 17.4 MB (32.72%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -4,10 +4,12 @@ class Solution:
         b = 0
         for letter in range(1, len(colors)): 
             if letter + 1 < len(colors):
-                if colors[letter - 1] == "A" and colors[letter] == "A" and colors[letter + 1] == "A":
-                    a += 1
-                if colors[letter - 1] == "B" and colors[letter] == "B" and colors[letter + 1] == "B":
-                    b +=1
+                if colors[letter - 1] == colors[letter] ==  colors[letter + 1] :
+                    if colors[letter] == "A":
+                        a +=1
+                    else:
+                        b += 1
+                
 
         if a > b:
             return True
