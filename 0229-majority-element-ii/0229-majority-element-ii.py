9971e94ffab2a7c19b133939e057784cea145c22
Adam Lehrer <50192895+AdamSL394@users.noreply.github.com>
2023-10-04 21:16:23 -0700

    Time: 118 ms (24.43%), Space: 17.7 MB (84.36%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,21 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        
-        N = len(nums)
-        
-        majority = N / 3
-        
-        dic = {}
-        
-        for num in nums:
-            if num not in dic:
-                dic[num] = 1
-            else: 
-                dic[num] += 1
+        freq = Counter(nums)
+
+        base = int(len(nums)/3)
         res = []
-        for k,v in dic.items():
-            if float(v) > majority:
+        
+        for k,v in freq.items():
+            if v > base:
                 res.append(k)
-                
-        return (res)
-                
+        return res
